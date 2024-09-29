abbfd153d0bbc656503f9867bfb41e26d7537634
Arvid Gerstmann <github@arvid.io>
2024-09-29 14:39:04 +0200

    next

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,114 @@
-#![allow(
-    missing_docs,
-    missing_debug_implementations,
-    clippy::missing_docs_in_private_items,
-    clippy::missing_panics_doc
-)]
-//! Rust Runtime Dependency Injection Framework.
-
-use std::any::TypeId;
+use std::any::{Any, TypeId};
 use std::collections::HashMap;
+use std::marker::PhantomData;
 use std::sync::Arc;
 
-pub mod test;
+use once_cell::sync::OnceCell;
+use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use thiserror::Error;
 
-enum Object {
-    Singleton(SingletonObject),
-    Transient(TransientObject),
+static DEFAULT_REGISTRY: OnceCell<RwLock<Registry>> = OnceCell::new();
+
+#[derive(Debug, Error)]
+pub enum RegistryError {
+    #[error("already a default registry set for this process")]
+    DefaultAlreadySet,
 }
 
-struct SingletonObject {
-    pub deps: Option<Vec<TypeId>>,
-    pub obj: Arc<dyn std::any::Any + Send + Sync>,
+pub trait Dep {
+    fn new(registry: &Registry) -> Self;
 }
 
-struct TransientObject {
-    pub deps: Option<Vec<TypeId>>,
-    pub ctor: Box<
-        dyn Fn(&Registry, &Option<Vec<TypeId>>) -> Box<dyn std::any::Any + Send + Sync>
-            + Send
-            + Sync,
-    >,
+pub struct Transient<T> {
+    inner: T,
 }
 
-#[non_exhaustive]
-pub enum Dependency {
-    Empty,
-    Singleton(Arc<dyn std::any::Any + Send + Sync>),
-    Transient(Box<dyn std::any::Any + Send + Sync>),
-}
-
-impl Dependency {
-    pub fn transient<T: 'static>(self) -> T {
-        if let Self::Transient(transient) = self {
-            return *transient.downcast::<T>().expect("bug");
-        }
-
-        unreachable!("bug")
-    }
-
-    pub fn singleton<T: Send + Sync + 'static>(&self) -> Arc<T> {
-        if let Self::Singleton(singleton) = self {
-            return Arc::clone(singleton).downcast::<T>().expect("bug");
-        }
-
-        unreachable!("bug")
+impl<T: Send + Sync + 'static> Transient<T> {
+    pub fn get(self) -> T {
+        self.inner
     }
 }
 
+impl<T: Send + Sync + 'static> Dep for Transient<T> {
+    fn new(registry: &Registry) -> Self {
+        Self {
+            inner: registry.get_transient::<T>().unwrap(),
+        }
+    }
+}
+
+pub struct Singleton<T> {
+    inner: Arc<T>,
+}
+
+impl<T: Send + Sync + 'static> Singleton<T> {
+    pub fn get(self) -> Arc<T> {
+        self.inner
+    }
+}
+
+impl<T: Send + Sync + 'static> Dep for Singleton<T> {
+    fn new(registry: &Registry) -> Self {
+        Self {
+            inner: registry.get_singleton::<T>().unwrap(),
+        }
+    }
+}
+
+pub trait DepBuilder<R> {
+    fn build(registry: &Registry, ctor: fn(Self) -> R) -> R;
+    fn as_typeids(&self) -> Vec<TypeId>;
+}
+
+impl<R: Send + Sync + 'static> DepBuilder<R> for () {
+    fn build(_registry: &Registry, ctor: fn(Self) -> R) -> R {
+        ctor(())
+    }
+
+    fn as_typeids(&self) -> Vec<TypeId> {
+        Vec::new()
+    }
+}
+
+macro_rules! DepBuilderImpl {
+    ($n:expr, { $($ts:ident),+ }, $ty:ty) => {
+        impl<R: Send + Sync + 'static, $($ts: Dep + Send + Sync + 'static,)*> DepBuilder<R> for $ty {
+            fn build(registry: &Registry, ctor: fn(Self) -> R) -> R {
+                ctor(
+                    (
+                        $(
+                            <$ts>::new(registry),
+                        )*
+                    )
+                )
+            }
+
+            fn as_typeids(&self) -> Vec<TypeId> {
+                vec![ $(TypeId::of::<$ts>(),)* ]
+            }
+        }
+    };
+}
+
+DepBuilderImpl!(1, { T1 }, (T1,));
+DepBuilderImpl!(2, { T1, T2 }, (T1, T2));
+DepBuilderImpl!(3, { T1, T2, T3 }, (T1, T2, T3));
+
+type BoxedCtor = Box<dyn Fn(&Registry) -> Box<dyn Any + Send + Sync> + Send + Sync>;
+
+pub enum Object {
+    Transient(BoxedCtor),
+    Singleton(Arc<dyn Any + Send + Sync>),
+}
+
+#[derive(Default)]
 pub struct Registry {
-    /// Singletons live for the entire registry live time.
     objects: HashMap<std::any::TypeId, Object>,
 }
 
 impl Registry {
-    pub fn new() -> Self {
-        Self {
+    pub fn new() -> Registry {
+        Registry {
             objects: HashMap::new(),
         }
     }
@@ -74,55 +119,27 @@ impl Registry {
     {
         self.objects.insert(
             TypeId::of::<T>(),
-            Object::Transient(TransientObject {
-                deps: None,
-                ctor: Box::new(move |_, _| -> Box<dyn std::any::Any + Send + Sync> {
-                    let obj = ctor();
-                    Box::new(obj)
-                }),
-            }),
+            Object::Transient(Box::new(move |_| -> Box<dyn Any + Send + Sync> {
+                let obj = ctor();
+                Box::new(obj)
+            })),
         );
     }
 
-    pub fn transient_deps<T, D>(&mut self, ctor: fn(Vec<Dependency>) -> T, deps: D)
+    pub fn singleton<T>(&mut self, singleton: T)
     where
         T: Send + Sync + 'static,
-        D: Dependable,
     {
-        self.objects.insert(
-            TypeId::of::<T>(),
-            Object::Transient(TransientObject {
-                deps: Some(make_deps(deps)),
-                ctor: Box::new(move |this, deps| -> Box<dyn std::any::Any + Send + Sync> {
-                    let deps = deps.as_ref().expect("this is a bug");
-                    let mut obj_deps = Vec::<Dependency>::with_capacity(deps.len());
-                    for dep in deps {
-                        if let Some(transient) = this.get_transient_dyn(dep) {
-                            obj_deps.push(Dependency::Transient(transient));
-                        } else if let Some(singleton) = this.get_singleton_dyn(dep) {
-                            obj_deps.push(Dependency::Singleton(singleton));
-                        }
-                    }
-                    let obj = ctor(obj_deps);
-                    Box::new(obj)
-                }),
-            }),
-        );
+        self.objects
+            .insert(TypeId::of::<T>(), Object::Singleton(Arc::new(singleton)));
     }
 
-    pub fn singleton<T: Send + Sync + 'static>(&mut self, singleton: T) {
-        self.objects.insert(
-            TypeId::of::<T>(),
-            Object::Singleton(SingletonObject {
-                deps: None,
-                obj: Arc::new(singleton),
-            }),
-        );
-    }
-
-    pub fn get_transient<T: Send + Sync + 'static>(&self) -> Option<T> {
-        if let Some(Object::Transient(transient)) = self.objects.get(&TypeId::of::<T>()) {
-            let boxed = (transient.ctor)(self, &transient.deps);
+    pub fn get_transient<T>(&self) -> Option<T>
+    where
+        T: Send + Sync + 'static,
+    {
+        if let Some(Object::Transient(ctor)) = self.objects.get(&TypeId::of::<T>()) {
+            let boxed = (ctor)(self);
             if let Ok(obj) = boxed.downcast::<T>() {
                 return Some(*obj);
             }
@@ -131,88 +148,74 @@ impl Registry {
         None
     }
 
-    pub fn get_transient_dyn(
-        &self,
-        typeid: &TypeId,
-    ) -> Option<Box<dyn std::any::Any + Send + Sync>> {
-        if let Some(Object::Transient(transient)) = self.objects.get(typeid) {
-            let boxed = (transient.ctor)(self, &transient.deps);
-            return Some(boxed);
-        }
-
-        None
-    }
-
-    pub fn get_singleton<T: Send + Sync + 'static>(&self) -> Option<Arc<T>> {
+    pub fn get_singleton<T>(&self) -> Option<Arc<T>>
+    where
+        T: Send + Sync + 'static,
+    {
         if let Some(Object::Singleton(singleton)) = self.objects.get(&TypeId::of::<T>()) {
-            if let Ok(typed_obj) = Arc::clone(&singleton.obj).downcast::<T>() {
-                return Some(typed_obj);
+            if let Ok(obj) = Arc::clone(singleton).downcast::<T>() {
+                return Some(obj);
             }
         }
 
         None
     }
 
-    pub fn get_singleton_dyn(
-        &self,
-        typeid: &TypeId,
-    ) -> Option<Arc<dyn std::any::Any + Send + Sync>> {
-        if let Some(Object::Singleton(singleton)) = self.objects.get(typeid) {
-            return Some(Arc::clone(&singleton.obj));
-        }
-
-        None
-    }
-}
-
-pub trait Dependable {
-    fn to_typeid(self, vec: &mut Vec<TypeId>);
-}
-
-#[derive(Debug)]
-pub struct Root;
-
-impl Root {
-    pub fn and<T: Send + Sync + 'static>(self) -> Dep<Self> {
-        Dep {
-            head: self,
-            args: TypeId::of::<T>(),
+    pub fn with_deps<T, Deps>(&mut self) -> Builder<T, Deps>
+    where
+        Deps: DepBuilder<T>,
+    {
+        Builder {
+            registry: self,
+            _marker: PhantomData,
+            _marker1: PhantomData,
         }
     }
-}
 
-impl Dependable for Root {
-    fn to_typeid(self, _vec: &mut Vec<TypeId>) {
-        // nothing to do
+    pub fn current() -> RwLockReadGuard<'static, Self> {
+        DEFAULT_REGISTRY.get().unwrap().read()
     }
-}
 
-#[derive(Debug)]
-pub struct Dep<H> {
-    head: H,
-    args: TypeId,
-}
+    pub fn current_mut() -> RwLockWriteGuard<'static, Self> {
+        DEFAULT_REGISTRY.get().unwrap().write()
+    }
 
-impl<H> Dep<H> {
-    pub fn and<T: Send + Sync + 'static>(self) -> Dep<Self> {
-        Dep {
-            head: self,
-            args: TypeId::of::<T>(),
+    pub fn make_current(self) -> Result<(), RegistryError> {
+        let res = DEFAULT_REGISTRY.set(RwLock::new(self));
+        if res.is_err() {
+            Err(RegistryError::DefaultAlreadySet)
+        } else {
+            Ok(())
         }
     }
 }
 
-pub fn make_deps<T: Dependable>(chain: T) -> Vec<TypeId> {
-    let mut ret = Vec::new();
-
-    chain.to_typeid(&mut ret);
-    ret
+pub struct Builder<'a, T, Deps> {
+    registry: &'a mut Registry,
+    _marker: PhantomData<T>,
+    _marker1: PhantomData<Deps>,
 }
 
-impl<H: Dependable> Dependable for Dep<H> {
-    fn to_typeid(self, vec: &mut Vec<TypeId>) {
-        self.head.to_typeid(vec);
-        vec.push(self.args);
+impl<'a, T, Deps> Builder<'a, T, Deps>
+where
+    Deps: DepBuilder<T> + 'static,
+    T: Send + Sync + 'static,
+{
+    pub fn transient(&mut self, ctor: fn(Deps) -> T) {
+        self.registry.objects.insert(
+            TypeId::of::<T>(),
+            Object::Transient(Box::new(move |this| -> Box<dyn Any + Send + Sync> {
+                let obj = Deps::build(this, ctor);
+                Box::new(obj)
+            })),
+        );
+    }
+
+    pub fn singleton(&mut self, ctor: fn(Deps) -> T) {
+        let obj = Deps::build(self.registry, ctor);
+        self.registry
+            .objects
+            .insert(TypeId::of::<T>(), Object::Singleton(Arc::new(obj)));
     }
 }
 
@@ -220,85 +223,103 @@ impl<H: Dependable> Dependable for Dep<H> {
 mod tests {
     use super::*;
 
-    #[derive(Debug, Default, PartialEq)]
-    struct TestObj {
-        pub str: String,
-        pub real: f64,
+    trait Env {
+        fn get(&self, var: &str) -> Option<String>;
+    }
+
+    struct RealEnv {}
+    impl Env for RealEnv {
+        fn get(&self, var: &str) -> Option<String> {
+            std::env::var(var).ok()
+        }
+    }
+
+    struct MockEnv {}
+    impl Env for MockEnv {
+        fn get(&self, _var: &str) -> Option<String> {
+            Some("TEST".to_owned())
+        }
     }
 
     #[test]
-    fn poc() {
+    fn test_new() {
         let mut registry = Registry::new();
-        registry.singleton(42_u16);
-        registry.transient(|| -> u8 { 42_u8 });
+        registry.transient(|| 1_u8);
 
-        let obj = registry.get_transient::<u8>();
-        assert_eq!(obj, Some(42_u8));
+        let x = registry.get_transient::<u8>();
+        assert_eq!(x, Some(1_u8));
 
-        registry.transient(TestObj::default);
+        registry
+            .with_deps::<_, (Transient<u8>,)>()
+            .transient(|(i,)| {
+                let i = i.get();
+                u16::from(i) + 1_u16
+            });
 
-        let obj2 = registry.get_transient::<TestObj>();
-        assert_eq!(
-            obj2,
-            Some(TestObj {
-                str: String::default(),
-                real: f64::default(),
-            })
-        );
+        let x1 = registry.get_transient::<u16>();
+        assert_eq!(x1, Some(2_u16));
 
-        let obj3 = registry.get_singleton::<u16>().unwrap();
-        assert_eq!(*obj3, 42_u16);
+        registry
+            .with_deps::<_, (Transient<u8>, Transient<u16>)>()
+            .transient(|(i, j)| {
+                let i = i.get();
+                let j = j.get();
+                u32::from(i) + u32::from(j) + 1_u32
+            });
+
+        let x2 = registry.get_transient::<u32>();
+        assert_eq!(x2, Some(4_u32));
+
+        registry.make_current().unwrap();
+
+        Registry::current_mut().transient(|| -1_i8);
+        let x3 = Registry::current().get_transient::<i8>();
+        assert_eq!(x3, Some(-1_i8));
+
+        Registry::current_mut().transient::<Box<dyn Env + Send + Sync>>(|| Box::new(MockEnv {}));
+        let env = Registry::current().get_transient::<Box<dyn Env + Send + Sync>>();
+        assert!(env.is_some());
+        assert_eq!(env.unwrap().get("anything"), Some("TEST".to_owned()));
     }
 
     #[test]
-    fn pocdep() {
+    fn singletons_without_deps() {
         let mut registry = Registry::new();
-        registry.transient(|| -> u8 { 42_u8 });
-        registry.transient_deps(
-            |mut deps| -> u16 {
-                let i = deps.pop().map(|el| el.transient::<u8>()).expect("bug");
-                u16::from(i) * 2
-            },
-            Root.and::<u8>(),
-        );
+        registry.transient(|| 1_u8);
+        registry.transient(|| 1_u16);
+        registry.transient(|| 1_u32);
+        registry.singleton(8_i8);
+        registry.singleton(16_i16);
+        registry.singleton(32_i32);
 
-        let obj = registry.get_transient::<u8>();
-        assert_eq!(obj, Some(42_u8));
-
-        let obj2 = registry.get_transient::<u16>();
-        assert_eq!(obj2, Some(84_u16));
+        let x1 = registry.get_singleton::<i8>();
+        assert_eq!(*x1.unwrap(), 8_i8);
+        let x2 = registry.get_singleton::<i16>();
+        assert_eq!(*x2.unwrap(), 16_i16);
+        let x3 = registry.get_singleton::<i32>();
+        assert_eq!(*x3.unwrap(), 32_i32);
     }
 
     #[test]
-    fn multiple_deps() {
+    fn singletons_with_deps() {
         let mut registry = Registry::new();
-        registry.transient(|| -> u8 { 42_u8 });
-        registry.transient_deps(
-            |mut deps| -> u16 {
-                let i = deps.pop().map(|el| el.transient::<u8>()).expect("bug");
-                u16::from(i) + 2
-            },
-            Root.and::<u8>(),
-        );
-        registry.transient_deps(
-            |mut deps| -> u32 {
-                let i16 = deps.pop().map(|el| el.transient::<u16>()).expect("bug");
-                let i8 = deps.pop().map(|el| el.transient::<u8>()).expect("bug");
-                u32::from(i16) + u32::from(i8) + 2
-            },
-            Root.and::<u8>().and::<u16>(),
-        );
+        registry.transient(|| 1_u8);
+        registry.singleton(8_i8);
 
-        let obj = registry.get_transient::<u32>();
-        assert_eq!(obj, Some(88_u32));
-    }
+        registry
+            .with_deps::<_, (Transient<u8>, Singleton<i8>)>()
+            .singleton(|(i, j)| {
+                let i = i.get();
+                let j = j.get();
+                i32::from(i) + i32::from(*j)
+            });
 
-    #[test]
-    #[allow(clippy::style)]
-    fn typeids() {
-        let x = Root.and::<u8>().and::<u16>();
-        let deps = make_deps(x);
-        assert_eq!(deps.get(0), Some(TypeId::of::<u8>()).as_ref());
-        assert_eq!(deps.get(1), Some(TypeId::of::<u16>()).as_ref());
+        let x1 = registry.get_transient::<u8>();
+        assert_eq!(x1.unwrap(), 1_u8);
+        let x2 = registry.get_singleton::<i8>();
+        assert_eq!(*x2.unwrap(), 8_i8);
+        let x3 = registry.get_singleton::<i32>();
+        assert_eq!(*x3.unwrap(), 9_i32);
+
     }
 }
