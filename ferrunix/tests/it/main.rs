00f41b9ca6893b3a9a65596fb39f4ff39d619c5d
Arvid Gerstmann <github@arvid.io>
2024-10-16 16:23:46 +0000

    chore: add more tests

--- ferrunix/tests/it/main.rs
+++ ferrunix/tests/it/main.rs
@@ -4,5 +4,6 @@ mod common;
 #[cfg(feature = "derive")]
 mod derive_simple;
 mod manual;
+mod manual_non_object_safe;
 mod manual_traits;
 mod validate_traits;
---
+++ ferrunix/tests/it/manual_non_object_safe.rs
@@ -0,0 +1,34 @@
+use ferrunix::Registry;
+
+#[derive(Debug, Default, PartialEq, PartialOrd, Hash)]
+struct ExampleStruct {
+    num: u32,
+    some_string: String,
+}
+
+trait NotObjectSafe {
+    fn returns(&self) -> Self; // ERROR: Self in return type
+}
+
+impl NotObjectSafe for ExampleStruct {
+    fn returns(&self) -> Self {
+        Self {
+            num: 1,
+            some_string: "2".to_owned(),
+        }
+    }
+}
+
+#[test]
+fn can_store_objecs_impl_non_object_safe_traits() {
+    let registry = Registry::empty();
+    registry.transient(|| ExampleStruct {
+        num: 0,
+        some_string: "1".to_owned(),
+    });
+
+    let example = registry.get_transient::<ExampleStruct>().unwrap();
+    let ret = example.returns();
+    assert_eq!(ret.num, 1);
+    assert_eq!(ret.some_string, "2");
+}
