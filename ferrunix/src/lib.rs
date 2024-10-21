28752a4979353b0a9fc688473799e140c6e5c497
Arvid Gerstmann <github@arvid.io>
2024-10-21 13:15:19 +0000

    better doc

--- ferrunix/src/lib.rs
+++ ferrunix/src/lib.rs
@@ -3,6 +3,7 @@
 pub use ferrunix_core::dependencies;
 pub use ferrunix_core::dependency_builder;
 pub use ferrunix_core::registry;
+pub use ferrunix_core::types;
 
 pub use dependencies::Singleton;
 pub use dependencies::Transient;
@@ -11,6 +12,8 @@ pub use registry::Registry;
 #[cfg(feature = "derive")]
 pub use ferrunix_macros::Inject;
 
+/// Register a [`RegistrationFunc`]. Usually invoked by the derive macro.
+///
 pub use ferrunix_core::registration::autoregister;
 pub use ferrunix_core::registration::RegistrationFunc;
 
