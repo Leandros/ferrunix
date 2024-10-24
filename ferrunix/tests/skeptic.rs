b487ad8b6acc0c89f3f60469682af6c466cecc96
Arvid Gerstmann <github@arvid.io>
2024-10-24 17:31:01 +0000

    fix skeptic warnings

--- ferrunix/tests/skeptic.rs
+++ ferrunix/tests/skeptic.rs
@@ -1,4 +1,14 @@
 //! Testing the book and `README.md`.
-#![allow(missing_docs)]
+#![allow(
+    missing_docs,
+    rustdoc::all,
+    clippy::unwrap_used,
+    clippy::all,
+    clippy::correctness,
+    clippy::perf,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::restriction
+)]
 
 include!(concat!(env!("OUT_DIR"), "/skeptic-tests.rs"));
