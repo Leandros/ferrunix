596aee1fd0e091fb5cd9e0428ad2f969f83fc5ed
Arvid Gerstmann <github@arvid.io>
2024-10-29 17:29:27 +0000

    refactor doc-tests

--- doc-tests/build.rs
+++ doc-tests/build.rs
@@ -1,10 +1,10 @@
-//! Build script for `ferrunix`.
+//! Build script for `ferrunix`'s doc tests.
 use skeptic::{generate_doc_tests, markdown_files_of_directory};
 
 fn main() {
     // Add all markdown files in directory "book/".
     let mut mdbook_files = markdown_files_of_directory("../book/");
     // Also add "README.md" to the list of files.
-    mdbook_files.push("README.md".into());
+    mdbook_files.push("../ferrunix/README.md".into());
     generate_doc_tests(&mdbook_files);
 }
---
+++ doc-tests/src/lib.rs
@@ -0,0 +1,1 @@
+//! Empty library, only necessary for testing the mdBook.
